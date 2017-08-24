5cc699bdad93b9c966d0744e7b87b0a36f0c9191
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-08-24 16:38:50 -0500

    cleaned up main.js to attempt to rebase

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,112 +1,9 @@
 "use strict";
 
-
-
 var callAPI = require("./api-interaction");
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Login Button Event Listener
-// $("#loginBtn").click(function(){
-//   console.log("clicked on Signin");
-//   user.logInGoogle()
-//   .then((result) => {
-//     console.log("result from login", result.user.uid);
-//     user.setUser(result.user.uid);
-//     $("auth-btn").addClass("is-hidden");
-//     $("#logout").removeClass("is-hidden");
-//     loadSongsToDOM();
-//   });
-// });
-
-// // Logout Button Event Listener
-// $("#logoutBtn").click(function(){
-//   console.log("logout clicked");
-//   user.logOut();
-//   loadSongsToDOM();
-
-// });
 console.log("something is working!");
 let searchString = "lord%20of%20the";
 console.log("searchString", searchString);
@@ -148,28 +45,6 @@ callAPI.searchMoviesAPI(searchString)
 })
 .then((movieArray) => {
 
-// console.log("castToGetId", castToGetId);
-
-
-// callAPI.getCastAPI(castToGetId).then((castResults) => {
-
-// 	console.log("castResults", castResults);
-// });
-
-
-// });
-
-	// console.log("movieArray", movieArray);
-
-	// $.each(movieArray, (key, value) => {
-	// 	let castToGetId = value.id;
-	// 	callAPI.getCastAPI(castToGetId).then((theResult) => {
-	// 	console.log("theResult", theResult);
-	// 	value.cast = theResult.cast;
-	// 	console.log("movieArray", movieArray);
-	// 	});
-	// });
-
 var promises = [];
 for (var i in movieArray) {
 	promises.push(callAPI.getCastAPI(movieArray[i].id));
@@ -198,42 +73,5 @@ Promise.all(promises).then((castArray) => {
 	function compileCastNames ()
 
 });
-// console.log("promises", promises);
 
-	// .then(() => {
-	// 	console.log("movieArray", movieArray);
-	// });
-
-	// let castToGetId = movieArray[0].id;
-	// console.log("castToGetId", castToGetId);
-
-	// callAPI.getCastApi(castToGetId);
-
-
-// });
-// .then((castResults) => {
-// 	console.log("castResults:", castResults);
-// });
-
-// callAPI.getCastAPI("120")
-// .then((castReturn) => {
-
-// console.log("castReturn", castReturn);
-	
 });
-// .then(() => {
-// 	console.log("movieArray", movieArray);
-// });
-
-
-
-// function searchMoviesAPI(searchString) {
-// 	return new Promise((resolve, reject) => {
-// 		console.log("what is the URL?", `https://api.themoviedb.org/3/search/movie?api_key=4542105ab96b56ba20c973e344b4ac55&language=en-US&query=${searchString}&page=1&include_adult=false`);
-// 		$.ajax({
-// 			url: `https://api.themoviedb.org/3/search/movie?api_key=4542105ab96b56ba20c973e344b4ac55&language=en-US&query=${searchString}&page=1&include_adult=false`
-// 		}).done((movieData) => {
-// 			resolve(movieData);
-// 		});
-// 	});
-// }
