24c9b16f0672f97c85dc5dd481c0a4e4dd6aef21
jordanberghofer <jordan.berghofer@gmail.com>
2017-08-24 11:56:52 -0500

    Comments for EventListeners in main.js and other styling things

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,48 @@
 "use strict";
 
+/***** Event Listeners *****/
+
+    /***NavBar***/
+
+        //Find movies enter button function id=""
+
+        //Find movies click button id="findMovie"
+
+        //Find my movies click button id="searchMyMovies"
+
+        //Login button click id="loginBtn"
+
+        //Logout button click id="logoutBtn"
+
+    /******/
+
+    /***Watched/Unwatched***/
+
+        //Click Funtion for Watched button
+
+        //Click Function for Unwatched button
+
+    /******/
+
+    /***Card Funtionality***/
+
+        //Add to watch list 
+        //If logged in push to database/else login alert
+
+        //Delete Card id="cardDltBtn"
+
+        //Rate Watched Movie
+
+    /******/
+
+
+
+
+
+
+
+
+
 
 
 
