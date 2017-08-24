d299313a2855d1cac2a653e08d26bdaf6a91387f
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-08-24 16:54:04 -0500

    test

---
+++ javascripts/api-interaction.js
@@ -0,0 +1,20 @@
+"use strict";
+// This module has no knowledge of the DOM, or where the data goes after it is fetched from Firebase.
+// It is only concerned with getting and setting data in the db
+
+// let $ = require('jquery'),
+
+function searchMoviesAPI(searchString) {
+	return new Promise((resolve, reject) => {
+		console.log("what is the URL?", `https://api.themoviedb.org/3/search/movie?api_key=4542105ab96b56ba20c973e344b4ac55&language=en-US&query=${searchString}&page=1&include_adult=false`);
+		$.ajax({
+			url: `https://api.themoviedb.org/3/search/movie?api_key=4542105ab96b56ba20c973e344b4ac55&language=en-US&query=${searchString}&page=1&include_adult=false`
+		}).done((movieData) => {
+			resolve(movieData);
+		});
+	});
+}
+
+
+
+module.exports = searchMoviesAPI;
