b6f809cc39879a7410b38e2f8a94920b3d66a687
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-08-25 10:06:47 -0500

    enclosed search in function

--- javascripts/searchAPI.js
+++ javascripts/searchAPI.js
@@ -6,82 +6,83 @@ var callAPI = require("./api-interaction");
 
 // console.log("something is working!");
 // let searchString = "lord%20of%20the";
+function submitAPISearch(searchString) {
+	searchString = "terminator";
 
-let searchString = "star%20wars";
-
-console.log("searchString", searchString);
-// let searchResults = callAPI.searchMoviesAPI(searchString);
-// console.log("searchResults", searchResults);
-
-// module.exports = callAPI;
-// console.log("callAPI", callAPI);
-
-let movieArray = [];
-
-callAPI.searchMoviesAPI(searchString)
-.then((searchResults) => {
+	console.log("searchString", searchString);
+	// let searchResults = callAPI.searchMoviesAPI(searchString);
 	// console.log("searchResults", searchResults);
-	// console.log("searchResults.results", searchResults.results);
 
-	$.each(searchResults.results, (key, value) => {
-		let movieObject = {};
-		movieObject.id = value.id;
-		movieObject.title = value.original_title;
-		movieObject.posterURL = "https://image.tmdb.org/t/p/w185" + value.poster_path;
-		movieObject.overview = value.overview;
-		movieObject.release_date = value.release_date;
-		movieArray.push(movieObject);
-	});
+	// module.exports = callAPI;
+	// console.log("callAPI", callAPI);
 
-	console.log("movieArray", movieArray);
-	return movieArray;
+	let movieArray = [];
 
-})
-.then((movieArray) => {
+	callAPI.searchMoviesAPI(searchString)
+	.then((searchResults) => {
+		// console.log("searchResults", searchResults);
+		// console.log("searchResults.results", searchResults.results);
 
-var promises = [];
-for (var i in movieArray) {
-	promises.push(callAPI.getCastAPI(movieArray[i].id));
-}
+		$.each(searchResults.results, (key, value) => {
+			let movieObject = {};
+			movieObject.id = value.id;
+			movieObject.title = value.original_title;
+			movieObject.posterURL = "https://image.tmdb.org/t/p/w185" + value.poster_path;
+			movieObject.overview = value.overview;
+			movieObject.release_date = value.release_date;
+			movieArray.push(movieObject);
+		});
 
-Promise.all(promises).then((creditsArray) => {
+		console.log("movieArray", movieArray);
+		return movieArray;
 
+	})
+	.then((movieArray) => {
 
-	// console.log("creditsArray", creditsArray);
-
-	let castNames = [];
-
-	$.each(creditsArray, (creditsKey, creditsValue) => {
-
-		let numberCast;
-		let eachMovieNames = [];
-
-		// console.log("creditsValue.cast", creditsValue.cast);
-
-		if (creditsValue.cast.length <= 5) {
-			numberCast = (creditsValue.cast.length);
-		} else {
-			numberCast = 5;
-		}
-
-		for(var k = 0; k < numberCast; k++) {
-			
-			// console.log("creditsValue.cast[k]", creditsValue.cast[k]);
-			// console.log("creditsValue.cast[k].name", creditsValue.cast[k].name);
-			eachMovieNames.push(creditsValue.cast[k].name);
-		}
-
-		castNames.push(eachMovieNames);
-	});
-
-	console.log("castNames at end of loop", castNames);
-
-	for (var k in movieArray) {
-		movieArray[k].cast = castNames[k];
+	var promises = [];
+	for (var i in movieArray) {
+		promises.push(callAPI.getCastAPI(movieArray[i].id));
 	}
 
+	Promise.all(promises).then((creditsArray) => {
 
-	console.log("movieArray after addition of cast names:", movieArray);
-});
 
-});
+		// console.log("creditsArray", creditsArray);
+
+		let castNames = [];
+
+		$.each(creditsArray, (creditsKey, creditsValue) => {
+
+			let numberCast;
+			let eachMovieNames = [];
+
+			// console.log("creditsValue.cast", creditsValue.cast);
+
+			if (creditsValue.cast.length <= 5) {
+				numberCast = (creditsValue.cast.length);
+			} else {
+				numberCast = 5;
+			}
+
+			for(var k = 0; k < numberCast; k++) {
+				
+				// console.log("creditsValue.cast[k]", creditsValue.cast[k]);
+				// console.log("creditsValue.cast[k].name", creditsValue.cast[k].name);
+				eachMovieNames.push(creditsValue.cast[k].name);
+			}
+
+			castNames.push(eachMovieNames);
+		});
+
+		console.log("castNames at end of loop", castNames);
+
+		for (var k in movieArray) {
+			movieArray[k].cast = castNames[k];
+		}
+
+
+		console.log("movieArray after addition of cast names:", movieArray);
+	});
+
+	});
+}
