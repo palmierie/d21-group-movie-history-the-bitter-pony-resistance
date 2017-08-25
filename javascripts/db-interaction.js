3879d27cbd86c86c7cc3528de6dfed46ae3d220f
Jim Vickery <jimrv8@yahoo.com>
2017-08-25 10:36:01 -0500

    added getMovies function

--- javascripts/db-interaction.js
+++ javascripts/db-interaction.js
@@ -5,7 +5,54 @@
 let firebase = require("./firebaseConfig");
 
 
+function getMovies(user) {
+    return new Promise((resolve, reject) => {
+        console.log('"url', firebase.getFBsettings().databaseURL);
+        $.ajax({
+            url: `${firebase.getFBsettings().databaseURL}/movies.json?orderBy="uid"&equalTo="${user}"`
+        }).done((movieData) => {
+            console.log('musicData inside of promise', movieData);
+            resolve(movieData);
+        }).fail((error) => {
+            reject(error);
+        });
+
+    });
+}
+
+function getLast(id) {
+    
+}
+
+function saveMovie() {
+
+}
+
+function getSavedMovies(uid) {
+
+}
+
+
+function deleteMovie(deleteId) {
+
+}
+
+function watchedMovie(editId) {
+
+}
+
+function rateMovie(editId) {
+
+}
+
+
 
 module.exports = {
- 
+    getMovies,
+    getLast,
+    saveMovie,
+    getSavedMovies,
+    deleteMovie,
+    watchedMovie,
+    rateMovie
 };
