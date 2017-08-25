629f554e9d9e950406c4f9294f952f0fce32e9e4
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-08-25 09:22:14 -0500

    promise.all function working

--- javascripts/searchAPI.js
+++ javascripts/searchAPI.js
@@ -1,26 +1,27 @@
 "use strict";
 
-require("./api-interaction");
+// require("./api-interaction");
 
 var callAPI = require("./api-interaction");
 
-console.log("something is working!");
-let searchString = "lord%20of%20the";
+// console.log("something is working!");
+// let searchString = "lord%20of%20the";
+
+let searchString = "star%20wars";
+
 console.log("searchString", searchString);
 // let searchResults = callAPI.searchMoviesAPI(searchString);
 // console.log("searchResults", searchResults);
 
 // module.exports = callAPI;
-console.log("callAPI", callAPI);
+// console.log("callAPI", callAPI);
 
 let movieArray = [];
 
 callAPI.searchMoviesAPI(searchString)
 .then((searchResults) => {
-	console.log("searchResults", searchResults);
-	console.log("searchResults.results", searchResults.results);
-
-
+	// console.log("searchResults", searchResults);
+	// console.log("searchResults.results", searchResults.results);
 
 	$.each(searchResults.results, (key, value) => {
 		let movieObject = {};
@@ -32,14 +33,7 @@ callAPI.searchMoviesAPI(searchString)
 		movieArray.push(movieObject);
 	});
 
-
-	// let castToGetId = movieArray[0].id;
-	// console.log("castToGetId", castToGetId);
-
-	// return castToGetId;
-
-	// console.log(movieArray)
-
+	console.log("movieArray", movieArray);
 	return movieArray;
 
 })
@@ -50,28 +44,44 @@ for (var i in movieArray) {
 	promises.push(callAPI.getCastAPI(movieArray[i].id));
 }
 
-Promise.all(promises).then((castArray) => {
+Promise.all(promises).then((creditsArray) => {
 
 
-	console.log("castArray", castArray);
+	// console.log("creditsArray", creditsArray);
 
-	for (var j in movieArray) {
-		movieArray[j].cast = castArray[j].cast;
+	let castNames = [];
+
+	$.each(creditsArray, (creditsKey, creditsValue) => {
+
+		let numberCast;
+		let eachMovieNames = [];
+
+		// console.log("creditsValue.cast", creditsValue.cast);
+
+		if (creditsValue.cast.length <= 5) {
+			numberCast = (creditsValue.cast.length);
+		} else {
+			numberCast = 5;
+		}
+
+		for(var k = 0; k < numberCast; k++) {
+			
+			// console.log("creditsValue.cast[k]", creditsValue.cast[k]);
+			// console.log("creditsValue.cast[k].name", creditsValue.cast[k].name);
+			eachMovieNames.push(creditsValue.cast[k].name);
+		}
+
+		castNames.push(eachMovieNames);
+	});
+
+	console.log("castNames at end of loop", castNames);
+
+	for (var k in movieArray) {
+		movieArray[k].cast = castNames[k];
 	}
 
-	console.log("movieArray with added cast", movieArray);
 
-	// for (var k in movieArray) {
-	// 	let castNames = [];
-	// 	let castToIterate = movieArray[k].cast;
-	// 	$.each(castToIterate, (key, value) => {
-	// 		castNames.push(value.name);
-	// 	});
-	// 	movieArray[k].castNames = castNames;
-	// }
-
-	// function compileCastNames ()
-
+	console.log("movieArray after addition of cast names:", movieArray);
 });
 
 });
