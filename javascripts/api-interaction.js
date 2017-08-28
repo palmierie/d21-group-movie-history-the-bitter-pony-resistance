34800dd4683fe4351f760a663f4d1a1365af7857
palmierie <ericmpalmieri@gmail.com>
2017-08-28 15:29:11 -0500

    got keys working finally

--- javascripts/api-interaction.js
+++ javascripts/api-interaction.js
@@ -1,33 +1,49 @@
 "use strict";
 
 console.log("api-interaction.js");
+let mdb = require("./mdb-getter.js"),
+		k = mdb();
 
-function searchMoviesAPI(searchString) {
+let ApiInteraction = {};
+
+var config = {
+							apiKey: k.apiKey,
+							authDomain: k.authDomain,
+							databaseURL: k.databaseURL
+};
+
+
+ApiInteraction.getMDBSettings = function() {
+	return config;
+};
+
+console.log('check for key', ApiInteraction.getMDBSettings());
+
+
+ApiInteraction.searchMoviesAPI = (searchString)=> {
 	return new Promise((resolve, reject) => {
-		// console.log("what is the movies URL?", `https://api.themoviedb.org/3/search/movie?api_key=4542105ab96b56ba20c973e344b4ac55&language=en-US&query=${searchString}&page=1&include_adult=false`);
 		$.ajax({
-			url: `https://api.themoviedb.org/3/search/movie?api_key=4542105ab96b56ba20c973e344b4ac55&language=en-US&query=${searchString}&page=1&include_adult=false`
+			url: `https://api.themoviedb.org/3/search/movie?api_key=${ApiInteraction.getMDBSettings().apiKey}&language=en-US&query=${searchString}&page=1&include_adult=false`
 		}).done((movieData) => {
 			resolve(movieData);
 		}).fail((error) => {
 			reject(error);
 		});
 	});
-}
+};
 
 
-function getCastAPI(movieId) {
+ApiInteraction.getCastAPI = (movieId)=> {
 	return new Promise((resolve, reject) => {
-		// console.log("what is the cast URL?", `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=4542105ab96b56ba20c973e344b4ac55`);
 		$.ajax({
-			url: `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=4542105ab96b56ba20c973e344b4ac55`
+			url: `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${ApiInteraction.getMDBSettings().apiKey}`
 		}).done((creditsData) => {
 			resolve(creditsData);
 		}).fail((error) => {
 			reject(error);
 		});
 	});
-}
+};
 
-module.exports = {searchMoviesAPI, getCastAPI};
+module.exports = ApiInteraction;
 
